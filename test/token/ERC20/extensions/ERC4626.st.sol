c482822107b89e49a335e32d5a0ab65e14c86a86
Daejun Park <daejunpark@gmail.com>
2022-10-21 15:59:48 -0700

    Add ERC4626 standard property tests

---
+++ test/token/ERC20/extensions/ERC4626.st.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "erc4626-tests/ERC4626.test.sol";
+
+import {ERC20Mock} from "../../../../contracts/mocks/ERC20Mock.sol";
+import {ERC4626Mock, IERC20Metadata} from "../../../../contracts/mocks/ERC4626Mock.sol";
+
+contract ERC4626StdTest is ERC4626Test {
+
+    function setUp() public override {
+        __underlying__ = address(new ERC20Mock("MockERC20", "MockERC20", address(this), 0));
+        __vault__ = address(new ERC4626Mock(IERC20Metadata(__underlying__), "MockERC4626", "MockERC4626"));
+        __delta__ = 0;
+    }
+
+}
